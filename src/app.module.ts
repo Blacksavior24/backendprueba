6a64cca3a46ec89ff84f28b916b5b70cbb47f543
Emerson Edward Villalta Quispe <reaperfocs@gmail.com>
2023-11-27 05:47:09 -0500

    backedn prod

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'notes',
+      type: 'postgres',
+      host: 'dpg-cli74vfjc5ks73eqkncg-a',
+      port: 5432,
+      username: 'db_crud_1j7b_user',
+      password: 'SzaPxtqmL6lZYaId4lLrQFMIZ2bftgvU',
+      database: 'db_crud_1j7b',
       autoLoadEntities: true,
-      synchronize: true
+      synchronize: true,
     }),
     TasksModule
   ],
